3497facfe4451f02e8a556b1ded3346247b20e9a
agustinaaylensolis <agustinaasolis04@gmail.com>
2025-11-03 10:14:15 -0300

    reporterepository

---
+++ Datos/Implementacion/ReporteRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos.DBContext;
+using Datos.Interfaces;
+using Entidades.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Datos.Implementacion
+{
+    public class ReporteRepository : IReporteRepository
+    {
+        private readonly SupercarpiDbContext _context;
+
+        public ReporteRepository(SupercarpiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Venta>> ObtenerVentasPorPeriodoAsync(DateTime desde, DateTime hasta)
+        {
+            return await _context.Venta
+                .Include(v => v.DetalleVenta)
+                    .ThenInclude(dv => dv.Producto)
+                .Include(v => v.MetodoPago)
+                .Include(v => v.Empleado)
+                .Where(v => v.Fecha >= desde && v.Fecha <= hasta)
+                .ToListAsync();
+        }
+
+    }
+}
---
+++ Datos/Interfaces/IReporteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades.Models;
+
+namespace Datos.Interfaces
+{
+    public interface IReporteRepository
+    {
+        Task<List<Venta>> ObtenerVentasPorPeriodoAsync(DateTime desde, DateTime hasta);
+    }
+}
